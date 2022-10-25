dcf17da8bc5598a4ec4167a7d4efe6dc1322ff29
YongHwanjae <ist635@naver.com>
2022-10-25 17:44:07 +0900

    1025

---
+++ project2/src/main/java/edu/kh/project/member/model/dao/MemberDAO.java
@@ -0,0 +1,24 @@
+package edu.kh.project.member.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.kh.project.member.model.vo.Member;
+
+@Repository // 퍼시스턴스 레이어, 영속성을 (파일, DB) 가진 클래스 + bean등록 
+public class MemberDAO {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	/** 로그인 DAO
+	 * @param memberEmail
+	 * @return loginMember
+	 */
+	public Member login(String memberEmail) {
+
+		return sqlSession.selectOne("", memberEmail);
+	}
+
+}
